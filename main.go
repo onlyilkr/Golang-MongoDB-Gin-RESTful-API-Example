922032cf8c112a9cc5b18d060056bb8fcc5b04d5
İlker Güldalı <teknolojik@yandex.com>
2021-10-16 01:49:45 +0300

    "registered" field's type edited

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -34,23 +35,23 @@ func main() {
 }
 
 type User struct {
-	UID           string  `bson:"_uid" json:"_uid"`
-	ID            int     `bson:"id" json:"id"`
-	IsActive      bool    `bson:"isActive" json:"isActive"`
-	Balance       string  `bson:"balance" json:"balance"`
-	Picture       string  `bson:"picture" json:"picture"`
-	Age           int     `bson:"age" json:"age"`
-	Name          string  `bson:"name" json:"name"`
-	Gender        string  `bson:"gender" json:"gender"`
-	Company       string  `bson:"company" json:"company"`
-	Email         string  `bson:"email" json:"email"`
-	Phone         string  `bson:"phone" json:"phone"`
-	Address       string  `bson:"address" json:"address"`
-	About         string  `bson:"about" json:"about"`
-	Registered    string  `bson:"registered" json:"registered"`
-	Latitude      float64 `bson:"latitude" json:"latitude"`
-	Longitude     float64 `bson:"longitude" json:"longitude"`
-	FavoriteFruit string  `bson:"favoriteFruit" json:"favoriteFruit"`
+	UID           string             `bson:"_uid" json:"_uid"`
+	ID            int                `bson:"id" json:"id"`
+	IsActive      bool               `bson:"isActive" json:"isActive"`
+	Balance       string             `bson:"balance" json:"balance"`
+	Picture       string             `bson:"picture" json:"picture"`
+	Age           int                `bson:"age" json:"age"`
+	Name          string             `bson:"name" json:"name"`
+	Gender        string             `bson:"gender" json:"gender"`
+	Company       string             `bson:"company" json:"company"`
+	Email         string             `bson:"email" json:"email"`
+	Phone         string             `bson:"phone" json:"phone"`
+	Address       string             `bson:"address" json:"address"`
+	About         string             `bson:"about" json:"about"`
+	Registered    primitive.DateTime `bson:"registered" json:"registered"`
+	Latitude      float64            `bson:"latitude" json:"latitude"`
+	Longitude     float64            `bson:"longitude" json:"longitude"`
+	FavoriteFruit string             `bson:"favoriteFruit" json:"favoriteFruit"`
 }
 
 func InitMongoDB() {
